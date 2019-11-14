ac5c8316d18cb438b97aa915c4fbf6c4110d32f8
User <User@User-OSX-3.local>
2019-11-14 13:13:35 -0500

    Added support for hash urls. Will load a specific iew when supplied a hash in the url, and will change the hash in the url when changing roots.

--- vino.js
+++ vino.js
@@ -14,9 +14,13 @@ async function newView(name, filePath) {
     return view;
 }
 // displays a view of a given name
-function display(viewName) {
-    const found = views.find(v => v.name == viewName);
-    document.getElementById("$vino").innerHTML = found.content;
+function display(viewName, changeHash = true) {
+    if (!changeHash) {
+        const found = views.find(v => v.name == viewName);
+        document.getElementById("$vino").innerHTML = found.content;
+    } else {
+        window.location.hash = "#" + viewName;
+    }
 }
 
 // sets up the vino pages app
@@ -25,5 +29,17 @@ async function vinoStart(vs) {
     // merge the arrays
     Array.prototype.push.apply(views, vs);
     const def = document.getElementById("$vino").getAttribute("vino-default");
-    display(def);
+    // load either the default view or the supplied hash url
+    if (!window.location.hash) {
+        window.location.hash = "#" + def;
+    } else {
+        hashChanged();
+    }
 }
+// is called when the hash in the url is changed,
+// and then displays the correct view
+function hashChanged() {
+    var hash = window.location.hash.substring(1);
+    display(hash, false);
+}
+window.onhashchange = hashChanged;
